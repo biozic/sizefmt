eadf3a99ef2b2dde0b4d588dd5d220e811d4493c
Nicolas Sicard <dransic@gmail.com>
2014-08-24 11:21:39 +0200

    Simplifications.

--- source/sizefmt.d
+++ source/sizefmt.d
@@ -8,21 +8,10 @@ enum Binary
     no = false
 }
 
-struct Config
+struct SizeBase(Binary binary = Binary.no,
+                string symbol = "B",
+                string space = " ")
 {
-    Binary binary = Binary.no;
-    string bytesSymbol = "B";
-    string byteWordSingular = "byte";
-    string bytewordPlural = "bytes";
-    string space = " ";
-    string[] decimalPrefixes = ["k", "M", "G", "T", "P", "E", "Z", "Y"];
-    string[] binaryPrefixes = ["Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi"];
-}
-
-struct SizeBase(alias config)
-{
-    static assert(is(typeof(config) == Config));
-
     ulong size;
 
     void toString(scope void delegate(const(char)[]) sink, FormatSpec!char fmt) const
@@ -31,9 +20,12 @@ struct SizeBase(alias config)
         enforce("sfFeEgGaA".canFind(fmt.spec), new FormatException(
             "Invalid floating point format specification: " ~ fmt.spec));
           
-        auto base = config.binary ? 1024.0 : 1000.0;
-        auto prefixes = config.binary ? config.binaryPrefixes : config.decimalPrefixes;
- 
+        auto base = binary ? 1024.0 : 1000.0;
+        static prefixes = binary
+            ? ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi", "Yi"]
+            : ["", "k", "M", "G", "T", "P", "E", "Z", "Y"];
+
+        // TODO: Calculate the order. This or a log?
         int order = 0;
         double tmp = size;
         while (tmp > (base - 1))
@@ -41,35 +33,27 @@ struct SizeBase(alias config)
             order++;
             tmp = tmp / base;
         }
+
         order = min(order, prefixes.length);
 
-        if (order == 0)
-        {
-            sink.formatValue(cast(double) size, fmt);
-            sink(config.space);
-            sink(size == 1 ? config.byteWordSingular : config.bytewordPlural);
-        }
-        else
-        {
-            sink.formatValue(size / base^^order, fmt);
-            sink(config.space);
-            sink(prefixes[order - 1]);
-            sink(config.bytesSymbol);
-        }
+        sink.formatValue(size / base^^order, fmt);
+        sink(space);
+        sink(prefixes[order]);
+        sink(symbol);
     }
 }
 
-alias Size = SizeBase!(Config(Binary.no));
-alias BinSize = SizeBase!(Config(Binary.yes));
+alias Size = SizeBase!();
+alias BinSize = SizeBase!(Binary.yes);
 
 version(unittest)
     import std.string;
 
 unittest
 {
-    assert("%s".format(Size(1)) == "1 byte");
-    assert("%s".format(Size(42)) == "42 bytes");
-    assert("%s".format(Size(999)) == "999 bytes");
+    assert("%s".format(Size(1)) == "1 B");
+    assert("%s".format(Size(42)) == "42 B");
+    assert("%s".format(Size(999)) == "999 B");
     assert("%s".format(Size(1000)) == "1 kB");
     assert("%.1f".format(Size(862_558_172_533)) == "862.6 GB");
     assert("%.2f".format(Size(ulong.max)) == "18.45 EB");
@@ -80,13 +64,7 @@ unittest
 
 unittest
 {
-    enum Config config = {
-        bytesSymbol:"o",
-        byteWordSingular:"octet",
-        bytewordPlural:"octets"
-    };
-    alias MySize = SizeBase!config;
-
-    assert("%s".format(MySize(42)) == "42 octets");
+    alias MySize = SizeBase!(Binary.no, "o");
+    assert("%s".format(MySize(42)) == "42 o");
     assert("%.1f".format(MySize(862_558_172_533)) == "862.6 Go");
 }
